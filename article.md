2305ee03a7ffd16b1762ac02597ccebbed966fcb
Jan Boček <honza.bocek@gmail.com>
2018-03-27 20:11:08 +0200

    fotky

--- article.md
+++ article.md
@@ -1,41 +1,122 @@
-title: "Data z mobilů:<br>Praha se denně nafoukne o polovinu, v centru jsou návštěvníci v převaze"
-perex: "Skoro 140 tisíc lidí přijede do Prahy každý den za prací. Míří hlavně do kancelářských budov a do centra města. S nimi pak dorazí i přes půl milionu návštěvníků. Ukázala to analýza dat o pohybu mobilních telefonů, kterou si nyní město nechává zpracovat."
-authors: ["Jan Cibulka", "Petr Kočí"]
-published: "21. dubna 2018"
+title: "XXX"
+perex: "XXX"
+authors: ["Jan Boček", "Jan Cibulka"]
+published: "29. března 2018"
 coverimg: https://interaktivni.rozhlas.cz/brexit/media/cover.jpg
 coverimg_note: "Foto <a href='#'>ČTK</a>"
 styles: []
-libraries: [] #jquery, jquery-csv, d3, highcharts, datatables
-options: [noheader] #wide, noheader (, nopic)
+libraries: [jquery, highcharts, 'https://code.highcharts.com/modules/annotations.js']
+options: []
 ---
-<left>
-	<p>
-	<b>KAREL HYNEK MÁCHA</b>
-	</p><p>
-	Karel Hynek Mácha (16. listopadu 1810 Praha-Malá Strana[1] – 6. listopadu 1836 Litoměřice[2]) byl český básník a prozaik, představitel českého romantismu a zakladatel moderní české poezie. Proslavil se jak svým životem, tak dílem, jemuž dominuje Máj (1836).
-	</p>
-</left>
 
-Během dne se lidé v Praze soustřeďují v okolí administrativních center a obchodů, po poledni se tak nejvíc zahustí okolí Andělu, centrum Prahy (Vodičkova ulice a Petrské náměstí) a Brumlovka nedaleko Budějovické. Na poslední jmenované adrese sídlí řada velkých korporací, mezi nimi i Microsoft nebo ČEZ. Pro srovnání, na zmíněných místech je ve „špičce“ okolo šesti stovek osob na jeden hektar, průměr Prahy je 25 obyvatel na [hektar](https://cs.wikipedia.org/wiki/Hektar) (Václavské náměstí má rozlohu asi 4 hektary).
+## Jaké jsou nejčastější skautské přezdívky?
 
-Naopak v noci se nejvíc lidí se „namačká“ ve Vršovicích, jde o 430 osob na hektar.
+TADY BUDE SEARCHBOX
 
-Detaily si můžete prohlédnout v následující mapě, kterou z dat mobilních operátorů zpracoval pražský [Institut plánování a rozvoje](http://www.iprpraha.cz/).
+<wide><div id="prezdivky"></div></wide>
 
-<wide>_Pokud vás zajímá, kolik lidí bývá ve dne a v noci ve vaší čtvrti, najděte si ji na mapě, přepněte se na časové řezy, vyberte přepínač hustota osob a potom pohybujte posuvníkem nahoře._</wide>
+NĚJAKÝ ÚVODNÍ TEXT, ASI HROZNÝŠ VČ. CITACE
 
+Data zároveň ukazují, jaké přezdívky byly populární v různých obdobích. Skauti narození ve dvacátých nebo třicátých letech minulého století mají nejčastěji jména postav z Kiplingovy Knihy džunglí: Mauglí, Akéla nebo Balů.
 
-## Data na prodej
-<right>
-	<p>
-	<b>KAREL HYNEK MÁCHA</b>
-	</p><p>
-	Karel Hynek Mácha (16. listopadu 1810 Praha-Malá Strana[1] – 6. listopadu 1836 Litoměřice[2]) byl český básník a prozaik, představitel českého romantismu a zakladatel moderní české poezie. Proslavil se jak svým životem, tak dílem, jemuž dominuje Máj (1836).
-	</p>
-</right>
+„Skautské aktivity mají vždy nějaký symbolický rámec,“ vysvětluje mluvčí českého skauta Barbora Trojak, sama Brambora. „V tomhle období se program vlčáků, to je přibližně desetiletých kluků, rámoval právě Knihou džunglí.
 
-I pokud netelefonujeme nebo neposíláme SMS, telefon pravidelně komunikuje s vysílači v síti mobilního operátora. Z dat o síle signálu a použité anténě je možné odvodit, kde se přibližně telefon nachází a jak dlouhou dobu na místě strávil.
+Skautské přezdívky ilustrují dějiny komplikovaného dvacátého století. V době německé okupace skauti zanikli, nakrátko byli obnoveni v roce 1945. Generace, která vstupovala do skauta po osvobození, si ráda nechala říkat anglicky znějícími jmény – Johny, Mary, George.
 
-Mobilní operátor má [zákonnou povinnost](https://www.zakonyprolidi.cz/cs/2005-127/zneni-20160919#p97-3) takové informace uchovávat, a to půl roku do minulosti, přistupovat k nim ale mohou jen bezpečnostní složky, typicky policie nebo rozvědka.
+Další příležitost dostalo skautské hnutí v roce 1968. Tentokrát jen na dva roky. Generace šedesátých let nicméně zanechala v datech stopu v podobě Rychlonožků, Bidel a Červenáčků.
 
-Operátoři ale obdobné informace mohou anonymizovat (tedy zbavit identifikace konkrétního uživatele) a agregovat, tedy spojit informace o jednotlivcích do jakýchsi skupin. Výsledná obecná čísla pak přeprodávají dál, například marketingovým firmám či dopravním analytikům. Zpětně z nich nejde dovodit, kde se pohybuje každý jednotlivec, dávají ale určitý přehled o obecném chování obyvatel nějakého místa.
+Po dosud posledním obnovení skauta v roce 1989 se zase začaly objevovat přezdívky z tolkienovských příběhů: Hobit, Glum, Frodo nebo Gandalf.
+
+<div style="width: 800px; overflow: hidden; display: block;"><div id="maugli"></div><div id="us"></div></div>
+<div style="width: 800px; overflow: hidden; display: block;"><div id="sipy"></div><div id="tolkien"></div></div>
+
+Během krátkého uvolnění v šedesátých letech do skautu vstoupila řada osobností. Mohlo se tam dát dohromady třeba divadlo Sklep: skautovat začal sklepácký režisér Václav Marhoul, herci Eva Holubová, David Vávra nebo Tomáš Hanák.
+
+CITACE TOMÁŠE HANÁKA
+
+Většina skautských přezdívek se ale neváže k žádnému historickému období a vychází ze sdílených zážitků. Nejčastějším zdrojem jsou zvířecí jména; vůbec nejobvyklejší skautskou přezdívkou je _Veverka_, nejoblíbenějšími ptačími přezdívkami jsou protichůdní _Orel_ a _Kuře_. Běžná je také rodina, pečivo nebo interpunkce.
+
+<div style="width: 800px; overflow: hidden; display: block;"><div id="rodina"></div><div id="ptaci"></div></div>
+<div style="width: 800px; overflow: hidden; display: block;"><div id="pecivo"></div><div id="interpunkce"></div></div>
+
+## XXX
+
+Během sedmdesátých a osmdesátých let, kdy byl skaut opět v ilegalitě, se jeho příznivci nejčastěji schovávali v různých sportovních a turistických organizacích nebo Svazarmu, sdružujícím sportovní svazy. Tímhle způsobem se ukrýval například Druhý pražský oddíl _Hoši od Bobří řeky_, v letech 1927 až 1987 vedený Jaroslavem Foglarem. Podobně přežívaly i další skupiny, vzdálené oficiální komunistické ideologii: třeba čeští indiáni.
+
+„Těsně před revolucí jsem vstoupil do indiánského oddílu – tedy kmene – Konestoga,“ vypráví ex-indián Petr Kočí s přezdívkou _Varan_. „Zasloužilí bojovníci měli komplikovaná jména v lakotštině. Samotného mě překvapuje, že si některá pamatuji dodnes. Náš náčelník se například jmenoval _Šagoiewatha_, další byli _Wanigiska_, _Mayašleča-nala_ nebo _Tewagion_. Šaman měl jméno _Lelašikah_ – _Ten, který kráčí proti proudu_.“
+
+Sám Kočí ovšem takové indiánské jméno nikdy nezískal. „Měl jsem asi málo orlích per, to byla u nás indiánů obdoba známějších bobříků,“ vzpomíná.
+
+FOTKA _Středoevropští indiáni na konci normalizace – novoročenka pražského kmene Sihasapa z roku 1989 Foto: archiv Petra Kočího_
+
+„Před revolucí jsme byli součástí Svazarmu,“ říká Kočí. „Pak jsme se začlenili do Junáka, ale vždycky jsme si od něj udržovali trochu odstup. Přišel mi tehdy jako polovojenská organizace a my indiáni jsme byli divočejší a svobodomyslnější, aspoň tak mi to tehdy připadalo.“
+
+Podobně jako kmen Konestoga se ke skautu po jeho obnovení přihlásila i řada dalších oddílů. Na počátku devadesátých let měl přes osmdesát tisíc členů.
+
+<wide><div id="clenove"></div></wide>
+
+Jenže porevoluční étos zároveň nadlouho zakonzervoval vývoj organizace. Po počátečním boomu začalo členů vytrvale ubývat a řada oddílů zanikla ještě do konce devadesátých let.
+
+„Junák v té době ještě neměl propracovaný a usazený systém vzdělávání, takže je možné, že se členové nesetkali se zajímavými a atraktivními aktivitami,“ vysvětluje skautská mluvčí Barbora Trojak. „Část členů to zřejmě odradilo a poohlédli se, co by mohli dělat jiného.“
+
+Propad skončil teprve v roce 2006 na 40 tisících členů.
+
+„V roce 2005 jsme přijali Chartu českého skautingu a začali systematicky pracovat na změně programové náplně,“ popisuje Trojak. „Měníme aktivity a témata, částečně i metody, i když základem už sto let zůstává skautská výchovná metoda. Hlavní ale bylo znovu oslovit děti a mladé lidi – zvlášť v době, kdy je na tomhle poli velká konkurence.“
+
+To se skautům podařilo. Od roku 2009, kdy mají k dispozici spolehlivá data o počtech členů, nejrychleji přibývalo vlčat a světlušek, tedy dětí mezi osmi a deseti lety. Před třemi lety navíc vznikla kategorie benjamínků pro děti od pěti do sedmi let.
+
+„Loni jsme na táborech napočítali téměř 400 tisíc děťodnů,“ dodává tisková mluvčí Junáka.
+
+<wide><div id="slozeni"></div></wide>
+
+„Postupně jsme se také dopracovali k systému vzdělávání dospívajících a dospělých, díky kterému se kluci a holky v oddílech setkávají s opravdu atraktivním programem reagujícím na vývoj a trendy ve společnosti,“ dodává Trojak.
+
+FOTKY ODBOREK _Jednou z činností, která se během sta let proměnila, jsou takzvané odborky, skautská varianta lovení bobříků. Před rokem 1918 se skládaly zkoušky z oborů jako_ chůva _a_ korespondentka, _dnes_ deskovkář, geocacher _nebo_ smartphonista.
+
+## XXX
+
+Skautské základny jsou po Česku rozprostřené poměrně rovnoměrně: nejhustší síť je logicky ve velkých městech, ale početně silné oddíly najdeme i v menších obcích.
+
+<wide><iframe src="https://dev.datarozhlas.cz/skauti-mapa/" width=100% height=800px frameborder=0></iframe></wide>
+
+Relativně nejrychleji skautů přibývá v regionu, který na tom v mnoha jiných ohledech, například [školství]( https://www.irozhlas.cz/zpravy-domov/analyza-regionalniho-skolstvi_1803140740_jab) nebo [nezaměstnanosti](https://interaktivni.rozhlas.cz/sudety/), není dobře – na Ústecku. Jen v posledním roce se počty skautů zvedly téměř o desetinu, v absolutních číslech 180 skautů.
+
+„Výhoda Sudet je v tom, že tu podobných aktivit je málo – na rozdíl třeba od Prahy tu nemáme téměř žádnou konkurenci a tak řada dětí přirozeně skončí u nás,“ vysvětluje Jakub Mráček, dlouholetý vedoucí teplického střediska Dvojka. Jen za poslední rok se jeho středisko rozrostlo o víc než dvacítku členů.
+
+„Do oddílu jsem přišel v osmnácti v roce 2003,“ popisuje krizi Junáka před patnácti lety. „Středisko tehdy vedl osmdesátiletý bard, ale byl naštěstí dost otevřený, aby je postupně přenechal mladším.“
+
+„V té době byl skaut na personálním dně,“ pokračuje. „Zároveň se odehrávala řada změn na celostátní úrovni – kdyby nebyly, asi bych tu nevydržel. Dnes věnujeme hodně energie vzdělávání vůdců, díky tomu máme prostor pro aktivity, které Junáka zatraktivňují a zviditelňují. Nedávno jsme třeba založili kmen dospělých, kteří se taky chtějí scházet.“
+
+Nejpočetnější jsou skauti v Jihomoravském kraji; je jich k devíti tisícům a jen loni přibylo víc než šest stovek dalších.
+
+Oproti tomu v Karlovarském kraji se počítají na stovky a jde také o jediný region, kde skautů nepřibývá.
+
+„Karlovarský kraj má mnoho specialit,“ vysvětluje Martin Dvořák z krajské rady Junáka. „Mezi ně patří absence dobrých středních a vysokých škol i špatná nabídka práce. Mnoho dětí odjede na střední školy do Plzně, Prahy, nebo dokonce Brna, a nevracejí se.
+
+„Právě stárnutí a vylidňování kraje je asi největší problém,“ pokračuje Dvořák. „Ostatně členové krajské rady sami pracují v jiných krajích, většinou v Praze. Což je velmi neobvyklé a komplikuje nám to práci.“
+
+Zanikající oddíly chtějí skauti vyvážit zakládáním oddílů „na zelené louce“. Snaží se vytipovat místa, kde je o skauting zájem a středisko zde chybí.
+
+„Našim hlavním cílem ale není, aby samo ústředí Junáka zakládalo nové jednotky,“ vysvětluje skautka Šárka Pavlásková. „Spíš chceme vytvořit co nejlepší podmínky a podporu pro všechny zájemkyně a zájemce, aby mohli sami založit nový oddíl.“
+
+„Jednu z největších výzev je změnit názor, že skautský oddíl může začít vést pouze někdo, kdo si sám skautingem prošel jako dítě,“ dodává Pavlásková.
+
+## XXX
+
+Řada českých skautských oddílů se hlásí ke křesťanství.
+
+CITACE BRAMBORY
+
+„Celosvětově jsou ale nejpočetnějším náboženstvím mezi skauty muslimové,“ říká tisková mluvčí Junáka Barbora Trojak. „Je to patrné na mezinárodních srazech, jamboree, kde se setkávají skauti ze všech koutů planety bez ohledu na víru. Sama jsem to zažila na anglickém jamboree v roce 2006. Když napršelo a všude bylo bláto, muslimové se za normálního provozu modlili v umývárkách.“
+
+V Česku zase před dvěma lety vznikl oddíl židovských skautů Tuvia.
+
+„Pro dítě z ortodoxní židovské rodiny je náročné až nemožné stát se plnou součástí oddílu, jezdit s nimi na výpravy, jezdit s nimi na tábory, protože o šábesu se necestuje a nerozdělává oheň, nevážou uzly, všechny skautské věci jsou vlastně zakázané a ještě se jí košer, což je při společném skautském vaření prakticky nemožné,“ [vysvětlila Českému rozhlasu](http://www.rozhlas.cz/nabozenstvi/zpravy/_zprava/novy-oddil-pro-zidovske-skauty-vznika-v-praze--1693997) vedoucí oddílu Hana Šiková.
+
+Oddíl navazuje na tradici prvorepublikového židovského skautingu. Cílem bylo vychovat skauty k odchodu do pouště a práci na novém židovském státu.
+
+„Byli tak úspěšní, že měli problém s udržením členské základny, protože se jim členové stěhovali do Palestiny a tím pádem jim ubývali tady,“ doplňuje Šiková.
+
+<wide><div style="overflow: hidden; box-sizing: border-box;"><div><img src="https://dev.datarozhlas.cz/olympiada-doping/fotky/grafy/koule.png" style="max-width: 100%; position: relative; float: left"/><div><img src="https://dev.datarozhlas.cz/olympiada-doping/fotky/grafy/disk.png" style="max-width: 100%; position: relative; float: left"/><div><img src="https://dev.datarozhlas.cz/olympiada-doping/fotky/grafy/400m.png" style="max-width: 100%; position: relative; float: left"/><div><img src="https://dev.datarozhlas.cz/olympiada-doping/fotky/grafy/maraton.png" style="max-width: 100%; position: relative; float: left"/></div></wide>
+
+<img src="https://dev.datarozhlas.cz/olympiada-doping/fotky/grafy/legenda.png">
--- js/script.js
+++ js/script.js
@@ -1,1 +1,845 @@
-// JS soubory ve složce /styles/ se do výsledného článku zakompilují automaticky
+var colors = ['#ECA038', '#A38456', '#EA614A', '#008836', '#20649B', '#6B96CA', '#A87A93', '#D19C95'];
+
+var pocetClenu = [
+  [1991,82537],
+  [1992,79337],
+  [1993,72088],
+  [1994,69313],
+  [1995,64484],
+  [1996,63464],
+  [1997,60087],
+  [1998,56979],
+  [1999,54989],
+  [2000,54543],
+  [2001,53028],
+  [2002,51264],
+  [2003,49851],
+  [2004,47863],
+  [2005,41967],
+  [2006,40583],
+  [2007,41844],
+  [2008,42349],
+  [2009,44638],
+  [2010,45252],
+  [2011,45586],
+  [2012,46687],
+  [2013,48093],
+  [2014,50438],
+  [2015,53028],
+  [2016,55533],
+  [2017,57840],
+  [2018,60522]
+];
+
+var prezdivky = [
+  ["Veverka",110],
+  ["Smíšek",84],
+  ["Beruška",79],
+  ["Myška",75],
+  ["Klíště",72],
+  ["Krtek",71],
+  ["Bobr",69],
+  ["Mája",68],
+  ["Pája",64],
+  ["Medvěd",63],
+  ["Ježek",63],
+  ["Míša",61],
+  ["Kiki",57],
+  ["Liška",57],
+  ["Tom",56],
+  ["Panda",56],
+  ["Kecka",54],
+  ["Ještěrka",51],
+  ["Žabka",50],
+  ["Cvrček",50]
+]
+
+var xdata = prezdivky.map(function(d) {
+        return d[0]
+    })
+
+var maugli = [
+  ["Akéla",27],
+  ["Balů",25],
+  ["Mauglí",22],
+  ["Ká",4],
+  ["Baghíra",2],
+  ["Šerchán",2],
+  ["Háthí",1]
+]
+
+var sipy = [
+  ["Bublina",32],
+  ["Rychlonožka",14],
+  ["Bidlo",13],
+  ["Červenáček",11],
+  ["Bohouš",10],
+  ["Rikitan",5],
+  ["Metelka",2],
+  ["Losna",2],
+  ["Široko",1],
+  ["Mirek Dušín",1],
+]
+
+var us = [
+  ["Jerry",39],
+  ["Johny",38],
+  ["Andy",31],
+  ["Harry",19],
+  ["Dave",18],
+  ["Mary",15],
+  ["Sid",13],
+  ["George",13],
+  ["Kitty",12],
+  ["Jack",12]
+]
+
+var tolkien = [
+  ["Smíšek",84],
+  ["Hobit",17],
+  ["Glum",13],
+  ["Frodo",12],
+  ["Gandalf",9],
+  ["Pipin",5],
+  ["Bilbo",4],
+  ["Legolas",3],
+  ["Thorin",2],
+  ["Gimli",2]
+]
+
+var xmaugli = maugli.map(function(d) {
+        return d[0]
+    })
+
+var xsipy = sipy.map(function(d) {
+        return d[0]
+    })
+
+var xus = us.map(function(d) {
+        return d[0]
+    })
+
+var xtolkien = tolkien.map(function(d) {
+        return d[0]
+    })
+
+var ptaci = [
+  ["Orel",34],
+  ["Kuře",34],
+  ["Sova",32],
+  ["Sojka",29],
+  ["Sokol",24],
+  ["Papoušek",21],
+  ["Datel",20],
+  ["Racek",18],
+  ["Kukačka",16],
+  ["Poštolka",15]
+]
+
+var rodina = [
+  ["Děda",11],
+  ["Teta",9],
+  ["Brácha",9],
+  ["Strejda",7],
+  ["Taťka",7],
+  ["Dědek",7],
+  ["Mamča",7],
+  ["Babča",6],
+  ["Omyl",3],
+  ["Synek",2]
+]
+
+var pecivo = [
+  ["Piškot",45],
+  ["Rohlík",8],
+  ["Houska",8],
+  ["Preclík",6],
+  ["Koblih",5],
+  ["Bageta",4],
+  ["Štrůdl",3],
+  ["Chleba",2],
+  ["Loupák",1],
+  ["Kaiserka",1]
+]
+
+var interpunkce = [
+  ["Tečka",13],
+  ["Otazník",11],
+  ["Čárka",3],
+  ["Vykřičník",2]
+]
+
+var xptaci = ptaci.map(function(d) {
+        return d[0]
+    })
+
+var xrodina = rodina.map(function(d) {
+        return d[0]
+    })
+
+var xpecivo = pecivo.map(function(d) {
+        return d[0]
+    })
+
+var xinterpunkce = interpunkce.map(function(d) {
+        return d[0]
+    })
+
+var benjaminci = [
+  ['rok 2009', 0],
+  ['rok 2018', 4688]
+]
+
+var vlcata = [
+  ['rok 2009', 15737],
+  ['rok 2018', 21451]
+]
+
+var skauti = [
+  ['rok 2009', 9067],
+  ['rok 2018', 13065]
+]
+
+var roveri = [
+  ['rok 2009', 7604],
+  ['rok 2018', 9195]
+]
+
+var dospeli = [
+  ['rok 2009', 5164],
+  ['rok 2018', 5546]
+]
+
+var ostatni = [
+  ['rok 2009', 7066],
+  ['rok 2018', 6577]
+]
+
+$(function () {
+
+    Highcharts.setOptions({
+            lang: {
+                months: ['leden', 'únor', 'březen', 'duben', 'květen', 'červen', 'červenec', 'srpen', 'září', 'říjen', 'listopad', 'prosinec'],
+                weekdays: ['neděle', 'pondělí', 'úterý', 'středa', 'čtvrtek', 'pátek', 'sobota'],
+                shortMonths: ['leden', 'únor', 'březen', 'duben', 'květen', 'červen', 'červenec', 'srpen', 'září', 'říjen', 'listopad', 'prosinec'],
+                thousandsSep: '',
+                decimalPoint:',',
+                rangeSelectorZoom: 'Zobrazit'
+            }
+        });
+
+Highcharts.chart('clenove', {
+
+    chart: {
+        type: 'area',
+        zoomType: 'x',
+        panning: true,
+        panKey: 'shift'
+    },
+
+    title: {
+        text: 'Junák, počet členů'
+    },
+
+    subtitle: {
+        text: ''
+    },
+
+    annotations: [{
+        labelOptions: {
+            backgroundColor: 'rgba(255,255,255,0.5)',
+            verticalAlign: 'top',
+            y: 15
+        },
+        labels: [{
+            point: {
+                xAxis: 0,
+                yAxis: 0,
+                x: 1997,
+                y: 52000
+            },
+            text: 'Generační obměna v 90. letech'
+        }]
+    }, {
+        labelOptions: {
+            shape: 'connector',
+            align: 'left',
+            justify: false,
+            crop: true,
+            style: {
+                fontSize: '0.8em',
+                textOutline: '1px white'
+            }
+        },
+        labels: [{
+            point: {
+                xAxis: 0,
+                yAxis: 0,
+                x: 1991,
+                y: 87000
+            },
+            text: '1989: Znovuzaložení Junáka'
+        }, {
+            point: {
+                xAxis: 0,
+                yAxis: 0,
+                x: 1992,
+                y: 79337
+            },
+            text: 'Návrat do mezinárodních organizací'
+        }, {
+            point: {
+                xAxis: 0,
+                yAxis: 0,
+                x: 1995,
+                y: 64484
+            },
+            text: 'Světové jamboree<br>Nizozemsko'
+        }, {
+            point: {
+                xAxis: 0,
+                yAxis: 0,
+                x: 2005,
+                y: 41967
+            },
+            text: 'Přijata Charta<br>českého skautingu'
+        }, {
+            point: {
+                xAxis: 0,
+                yAxis: 0,
+                x: 2007,
+                y: 41844
+            },
+            x: 60,
+            y: 35,
+            text: 'Světové jamboree<br>Velká Británie'
+        }, {
+            point: {
+                xAxis: 0,
+                yAxis: 0,
+                x: 2011,
+                y: 45586
+            },
+            x: 0,
+            y: -35,
+            text: 'Světové jamboree<br>Švédsko'
+        }]
+    }],
+
+    xAxis: {
+        labels: {
+            format: '{value}'
+        },
+        minRange: 5,
+        title: {
+            text: ''
+        }
+    },
+
+    yAxis: {
+        startOnTick: true,
+        endOnTick: false,
+        maxPadding: 0.35,
+        title: {
+            text: null
+        },
+        labels: {
+            format: '{value}'
+        },
+        title: {
+            text: 'počet členů'
+        }
+    },
+
+    tooltip: {
+        headerFormat: '<b>{point.y}</b> členů<br>',
+        pointFormat: '(rok {point.x})',
+        shared: true
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    credits: {
+        href : 'https://www.skaut.cz/',
+        text : 'Zdroj: Junák'
+    },
+
+    series: [{
+        data: pocetClenu,
+        lineColor: 'black',
+        color: '#294885',
+        fillOpacity: 0.5,
+        name: '',
+        marker: {
+            enabled: false
+        },
+        threshold: null
+    }]
+
+});
+
+Highcharts.chart('prezdivky', {
+
+    chart: {
+        type: 'column'
+    },
+
+    title: {
+        text: 'Junák, nejčastější přezdívky'
+    },
+
+    subtitle: {
+        text: ''
+    },
+
+    xAxis: {
+        categories: xdata
+    },
+
+    yAxis: {
+        title: {
+            text: ''
+        }
+    },
+
+    tooltip: {
+        pointFormat: '<b>{point.y}×</b>'
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'přezdívky',
+        data: prezdivky,
+        color: colors[0]
+    }]
+
+});
+
+Highcharts.chart('maugli', {
+
+    chart: {
+        type: 'column'
+    },
+
+    title: {
+        text: 'Kniha džunglí'
+    },
+
+    xAxis: {
+        categories: xmaugli
+    },
+
+    yAxis: {
+        title: ''
+    },
+
+    tooltip: {
+        pointFormat: '<b>{point.y}×</b>'
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'přezdívky',
+        data: maugli,
+        color: colors[1]
+    }]
+});
+
+Highcharts.chart('sipy', {
+
+    chart: {
+        type: 'column'
+    },
+
+    title: {
+        text: 'Rychlé šípy'
+    },
+
+    xAxis: {
+        categories: xsipy
+    },
+
+    yAxis: {
+        title: ''
+    },
+
+    tooltip: {
+        pointFormat: '<b>{point.y}×</b>'
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'přezdívky',
+        data: sipy,
+        color: colors[2]
+    }]
+});
+
+Highcharts.chart('us', {
+
+    chart: {
+        type: 'column'
+    },
+
+    title: {
+        text: 'Anglická jména'
+    },
+
+    xAxis: {
+        categories: xus
+    },
+
+    yAxis: {
+        title: ''
+    },
+
+    tooltip: {
+        pointFormat: '<b>{point.y}×</b>'
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'přezdívky',
+        data: us,
+        color: colors[3]
+    }]
+});
+
+Highcharts.chart('tolkien', {
+
+    chart: {
+        type: 'column'
+    },
+
+    title: {
+        text: 'Tolkien'
+    },
+
+    xAxis: {
+        categories: xtolkien
+    },
+
+    yAxis: {
+        title: ''
+    },
+
+    tooltip: {
+        pointFormat: '<b>{point.y}×</b>'
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'přezdívky',
+        data: tolkien,
+        color: colors[4]
+    }]
+});
+
+Highcharts.chart('ptaci', {
+
+    chart: {
+        type: 'column'
+    },
+
+    title: {
+        text: 'Ptáci'
+    },
+
+    xAxis: {
+        categories: xptaci
+    },
+
+    yAxis: {
+        title: ''
+    },
+
+    tooltip: {
+        pointFormat: '<b>{point.y}×</b>'
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'přezdívky',
+        data: ptaci,
+        color: colors[5]
+    }]
+});
+
+Highcharts.chart('rodina', {
+
+    chart: {
+        type: 'column'
+    },
+
+    title: {
+        text: 'Rodina'
+    },
+
+    xAxis: {
+        categories: xrodina
+    },
+
+    yAxis: {
+        title: ''
+    },
+
+    tooltip: {
+        pointFormat: '<b>{point.y}×</b>'
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'přezdívky',
+        data: rodina,
+        color: colors[6]
+    }]
+});
+
+Highcharts.chart('pecivo', {
+
+    chart: {
+        type: 'column'
+    },
+
+    title: {
+        text: 'Pečivo'
+    },
+
+    xAxis: {
+        categories: xpecivo
+    },
+
+    yAxis: {
+        title: ''
+    },
+
+    tooltip: {
+        pointFormat: '<b>{point.y}×</b>'
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'přezdívky',
+        data: pecivo,
+        color: colors[7]
+    }]
+});
+
+Highcharts.chart('interpunkce', {
+
+    chart: {
+        type: 'column'
+    },
+
+    title: {
+        text: 'Interpunkce'
+    },
+
+    xAxis: {
+        categories: xinterpunkce
+    },
+
+    yAxis: {
+        title: ''
+    },
+
+    tooltip: {
+        pointFormat: '<b>{point.y}×</b>'
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    legend: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'přezdívky',
+        data: interpunkce,
+        color: colors[0]
+    }]
+});
+
+Highcharts.chart('slozeni', {
+
+    chart: {
+        type: 'line'
+    },
+
+    title: {
+        text: 'Junák, členové podle věku'
+    },
+
+    subtitle: {
+        text: 'nejmladších přibývá nejrychleji'
+    },
+
+    xAxis: {
+        type: 'category',
+        categories: ['rok 2009', 'rok 2018']
+    },
+
+    yAxis: {
+        title: {
+            text: 'Počet členů'
+        }
+    },
+
+    tooltip: {
+        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b>'
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'benjamínci (5-7 let)',
+        data: benjaminci,
+        marker: {
+            symbol: 'circle'
+        },
+        color: colors[0]
+    }, {
+        name: 'vlčata/světlušky (7-11 let)',
+        data: vlcata,
+        marker: {
+            symbol: 'circle'
+        },
+        color: colors[1]
+    }, {
+        name: 'skauti/skautky (10-15 let)',
+        data: skauti,
+        marker: {
+            symbol: 'circle'
+        },
+        color: colors[2]
+    }, {
+        name: 'roveři/rangers (15-26 let)',
+        data: roveri,
+        marker: {
+            symbol: 'circle'
+        },
+        color: colors[3]
+    }, {
+        name: 'dospělí',
+        data: dospeli,
+        marker: {
+            symbol: 'circle'
+        },
+        color: colors[5]
+    }, {
+        name: 'ostatní',
+        data: ostatni,
+        marker: {
+            symbol: 'circle'
+        },
+        color: colors[6]
+    }]
+});
+
+});
